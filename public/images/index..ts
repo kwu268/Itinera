a7e3308120c41e0f65e11ae54ee2621005d4f592
Kelvin <kelvinwu717@gmail.com>
2025-07-28 22:52:22 -0400

    feat: add landing page

---
+++ public/images/index..ts
@@ -0,0 +1,3 @@
+export { default as aiConcept } from './aiConcept.png';
+export { default as homeScreen } from './homeScreen.png';
+export { default as airplane } from './airplane.jpg';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,157 @@
-import Image from "next/image";
+'use client';
+// import CornerLines from '@/components/shared/cornerLines';
+import React, { useEffect, useRef, useState } from 'react';
+import { homeScreen, aiConcept, airplane } from '../../public/images/index.';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isBottom, setIsBottom] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScroll = scrollRef.current;
+      if (!currentScroll) return;
+
+      setIsBottom(
+        currentScroll.scrollTop + currentScroll.clientHeight >=
+          currentScroll.scrollHeight - 1
+      );
+    };
+    const currentScroll = scrollRef.current;
+    if (currentScroll) currentScroll.addEventListener('scroll', handleScroll);
+    return () => {
+      if (currentScroll)
+        currentScroll.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  return (
+    <div
+      className="bg-gradient-to-br from-gray-900 via-black to-gray-800 text-white h-screen overflow-y-scroll snap-y snap-mandatory"
+      style={{ scrollBehavior: 'smooth' }}
+      ref={scrollRef}
+    >
+      {/* <CornerLines /> */}
+      <div className="h-screen flex flex-col justify-center items-center space-y-8 p-6 snap-start">
+        {/* Nav */}
+        <nav className="fixed top-0 left-0 p-6">
+          <div
+            className="font-bold text-lg tracking-wide cursor-pointer"
+            onClick={() =>
+              scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' })
+            }
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Itinera
+          </div>
+          <div className="md:hidden">
+            {/* Mobile menu icon */}
+            <button className="btn btn-sm btn-ghost">☰</button>
+          </div>
+        </nav>
+        {/* Hero Section */}
+        <section className="flex flex-col items-center space-y-3 text-center">
+          {/* heading */}
+          <h1 className="text-4xl md:text-6xl font-bold text-shadow-lg/40 text-shadow-amber-50 fade-in">
+            Welcome to Itinera
+          </h1>
+          {/* Desc */}
+          <p className="text-gray-300 text-lg md:text-xl">
+            Your one-stop solution for planning the perfect trip.
+          </p>
+          {/* buttons */}
+          <div className="flex  space-x-2 justify-center w-full text-md text-center">
+            <button className="btn btn-primary w-1/3 hover:bg-indigo-500">
+              Sign in
+            </button>
+            <button className="btn btn-outline w-1/3">Get Started</button>
+          </div>
+          {!isBottom ? (
+            <div className="absolute bottom-6 animate-pulse  text-sm text-gray-400">
+              ↓ Scroll to learn more
+            </div>
+          ) : <div
+            onClick={() => {
+              scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
+            }}
+            className="absolute bottom-6 animate-pulse  text-sm text-gray-400 cursor-pointer"
           >
-            Read our docs
-          </a>
+            ↑ Click here to jump back to the top
+          </div>}
+        </section>
+      </div>
+      {/* INFO Section */}
+      <section className="h-screen flex space-x-7 justify-center items-center px-4 py-12 text-center snap-start bg-gradient-to-br from-emerald-950 via-black to-grey-500 ">
+        <div className="  bg-gray-600/20 p-6 rounded-3xl h-1/2 flex flex-col justify-center">
+          <h2 className="text-3xl md:text-4xl font-semibold mb-4 opacity-100">
+            Create & Organize Trips
+          </h2>
+          <p className="max-w-xl text-gray-300">
+            Easily build vacation plans, schedule activities, and track weather
+            all in one place. With Itinera, you can seamlessly organize your
+            itinerary, add must-see attractions, and manage your travel schedule
+            with ease.
+          </p>
+          <p className="max-w-xl text-gray-300">
+            <br />
+            Enjoy a stress-free planning experience. Whether you’re exploring
+            new cities or revisiting favorite spots, Itinera helps you stay on
+            top of every aspect of your journey, making travel planning simple,
+            efficient, and enjoyable.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Image
+          src={homeScreen}
+          alt="Vacation Spot"
+          className="w-1/3 rounded-3xl"
+        />
+      </section>
+      <section className="h-screen flex space-x-7 justify-center items-center px-4 py-12 text-center snap-start bg-gradient-to-br from-red-950 via-black to-grey-500">
+        <Image
+          src={aiConcept}
+          alt="ai concept picture"
+          className="w-1/3 rounded-2xl"
+        />
+        <div className="bg-gray-600/20 p-6 rounded-3xl h-1/2 flex flex-col justify-center">
+          <h2 className="text-3xl md:text-4xl font-semibold mb-4">
+            AI-Powered Travel Assistant
+          </h2>
+          <p className="max-w-xl text-gray-300">
+            Discover the future of trip planning with Itinera’s AI-powered
+            assistant. Our intelligent feature helps you generate personalized
+            itineraries, recommends must-see attractions, and optimizes your
+            travel schedule based on your interests and preferences.
+            <br />
+            <br />
+            Simply tell the AI what kind of adventure you’re looking for, and
+            let it handle the details—from finding hidden gems to suggesting the
+            best routes and activities. Make your travel experience smarter,
+            easier, and more enjoyable with instant, tailored recommendations at
+            your fingertips.
+          </p>
+        </div>
+      </section>
+      <section className="h-screen flex space-x-7 justify-center items-center px-4 py-12 text-center snap-start bg-gradient-to-br from-indigo-950 via-black to-grey-500">
+        <div className="bg-gray-600/20 p-6 rounded-3xl h-1/2 flex flex-col justify-center items-center space-y-4">
+          <h1 className="text-4xl font-bold  text-shadow-amber-50 fade-in">
+            Unlock Your Ultimate Travel Experience
+          </h1>
+          <p className="max-w-xl text-gray-300">
+            Ready to take your adventures to the next level? Sign up for Itinera
+            and gain access to exclusive features designed to make your travel
+            planning effortless and enjoyable.
+            <br />
+            <br />
+            Join our community of explorers and start planning smarter,
+            traveling better, and making memories that last.
+          </p>
+          <button className="btn btn-primary w-1/3 hover:bg-indigo-500">
+            Sign up
+          </button>
+        </div>
+        <Image src={airplane} alt="airplane" className="w-2/5 h-1/2 rounded-3xl" />
+        
+      </section>
     </div>
   );
 }
